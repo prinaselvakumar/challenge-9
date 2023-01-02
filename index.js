463fad45cea240bd48368822ee4e2e7282aa4962
Prina Selvakumar <105674190+prinaselvakumar@users.noreply.github.com>
2023-01-02 16:43:35 -0600

    rearranged

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./generateMarkdown');
 
 
 
